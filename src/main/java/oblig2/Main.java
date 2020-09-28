f625bbcb4e559f1409162510b6eed6f43b582799
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-09-28 17:22:18 +0200

    Fikser konstruktøren slik at toString() fungerer. Oppgave 2a er ferdig

--- src/main/java/oblig2/Main.java
+++ src/main/java/oblig2/Main.java
@@ -2,6 +2,7 @@ package oblig2;
 
 public class Main {
     public static void main(String[] args) {
+
         String[] s1 = {};
         String[] s2 = {"A"};
         String[] s3 = {null, "A", null, "B", null};
@@ -11,6 +12,27 @@ public class Main {
         DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
         System.out.println(l1.toString() + " " + l2.toString() + " " + l3.toString()
                 + l1.omvendtString() + " " + l2.omvendtString() + " " + l3.omvendtString());
+
+
+        /*
+        String[] s3 = {null, "A", null, "B", null};
+        DobbeltLenketListe<String> liste = new DobbeltLenketListe<>(s3);
+        System.out.println(liste.toString() + "\n" + liste.omvendtString());
+
+        /*
+        for ( int i = 1; i <= 3; i++)
+        {
+            liste.leggInn(i);
+        }
+        System.out.println(liste.toString());
+        System.out.println(liste.omvendtString());
+
+         */
+// Utskrift:
+// [] []
+// [1] [1]
+// [1, 2] [2, 1]
+// [1, 2, 3] [3, 2, 1]
 
 
     }
